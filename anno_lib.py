078e22aa5801514f03a500f1f8d9b369a854cb24
Maswor <maswortxt@gmail.com>
2018-05-31 01:37:17 -0500

    done

--- anno_lib.py
+++ anno_lib.py
@@ -19,35 +19,6 @@ import codecs
 BOX_TYPE = Tuple[int, int, int, int, str]
 
 
-def create_corresponding_file(folder: pathlib.Path,
-                              corresponding_file: pathlib.Path,
-                              extension: str) -> pathlib.Path:
-    """ Given: foler ('User/Desktop/'), file ('/yolo/foobar.jpg')
-    extension('.xml'). Create: 'User/Desktop/foobar.xml"""
-    orig_name_without_suffix = corresponding_file.stem
-    new_name = orig_name_without_suffix + extension
-    new_file = folder / new_name
-
-    return new_file
-
-
-def write_pascal_voc(img_folder_name: str, img_file_name: str,
-                     img_shape: List[int], xml_file: str, img_path: str,
-                     boxes: List[BOX_TYPE]):
-    """ helper function to use PascalVocWriter class """
-    writer = PascalVocWriter(
-        img_folder_name,
-        img_file_name,
-        img_shape,
-        xmlFile=xml_file,
-        localImgPath=img_path)
-    writer.verified = False
-    for box in boxes:
-        x_1, y_1, x_2, y_2, label = box
-        writer.addBndBox(x_1, y_1, x_2, y_2, label, False)
-    writer.save(xml_file)
-
-
 class AnnoParser(metaclass=ABCMeta):
     """Abstract class for xml parser and matlab file parser"""
 
@@ -261,9 +232,9 @@ class AnnoWriter(metaclass=ABCMeta):
     @staticmethod
     def create_writer(parser_obj, binary, anno_format):
         """ Delegate parser base on extension """
-        if anno_format == 'csv':
+        if anno_format == '.csv':
             return CSVWriter(parser_obj, binary)
-        elif anno_format == 'xml':
+        elif anno_format == '.xml':
             return XMLWriter(parser_obj, binary)
         else:
             raise ValueError('Unsupported file type')
@@ -489,33 +460,57 @@ class PascalVocWriter:
         out_file.close()
 
 
+def create_corresponding_file(folder: pathlib.Path,
+                              corresponding_file: pathlib.Path,
+                              extension: str) -> pathlib.Path:
+    """ Given: foler ('User/Desktop/'), file ('/yolo/foobar.jpg')
+    extension('.xml'). Create: 'User/Desktop/foobar.xml"""
+    orig_name_without_suffix = corresponding_file.stem
+    new_name = orig_name_without_suffix + extension
+    new_file = folder / new_name
+
+    return new_file
+
+
+def write_pascal_voc(img_folder_name: str, img_file_name: str,
+                     img_shape: List[int], xml_file: str, img_path: str,
+                     boxes: List[BOX_TYPE]):
+    """ helper function to use PascalVocWriter class """
+    writer = PascalVocWriter(
+        img_folder_name,
+        img_file_name,
+        img_shape,
+        xmlFile=xml_file,
+        localImgPath=img_path)
+    writer.verified = False
+    for box in boxes:
+        x_1, y_1, x_2, y_2, label = box
+        writer.addBndBox(x_1, y_1, x_2, y_2, label, False)
+    writer.save(xml_file)
+
+
 def parse_args(args):
     """ Parsing a provided argument """
     parser = argparse.ArgumentParser(
         description="Simple parser script for infering Keras-Retinanet on image"
     )
     parser.add_argument(
-        'output_file', default='anno.csv', help="Location for csv file")
-    parser.add_argument('input_folder', help="Folder which has xml files")
+        'input',
+        help="Input annotation, either folder (of .xml) or file (.csv, .json)")
+    parser.add_argument(
+        'output',
+        help="Output annotation, either file (of .csv) or folder (.xml, .json)")
     parser.add_argument(
         '--binary', help="Binary annotation", action='store_true')
     return parser.parse_args(args)
 
 
-def main(args=None):
-    """ Main program, as a function to avoid setting up grobal variables """
-    args = parse_args(args)
-    my_parser = AnnoParser.create_parser('.csv', 'utf-8')
-    my_parser.set_anno_file('../sdsgwas/src/eval.csv')
-    my_data = my_parser.anno_data
-    my_writer = AnnoWriter.create_writer(
-        my_parser, binary=args.binary, anno_format='xml')
-    my_writer.set_output_folder('./test')
-    my_writer.write()
+def print_info(anno_data):
+    """ Printing boxes information """
     total_imgs = 0
     total_boxes = 0
     boxes_indices = [0 for _ in range(10)]
-    for img_file in my_data:
+    for img_file in anno_data:
         total_imgs += 1
         total_boxes += len(img_file['bbox'])
         for box in img_file['bbox']:
@@ -523,6 +518,50 @@ def main(args=None):
     print("There are {} images with {} boxes".format(total_imgs, total_boxes))
     print("Of which, number for DS score from 0 - 9 are: {}".format(
         boxes_indices))
+
+
+def select_parser(arg_input: str) -> AnnoParser:
+    """ Parser selection depending on arg_input """
+    m_input = pathlib.Path(arg_input)
+    if m_input.is_dir():
+        my_parser = AnnoParser.create_parser('.xml', 'utf-8')
+        my_parser.folder_path = arg_input
+    elif m_input.suffix == '.csv':
+        my_parser = AnnoParser.create_parser('.csv', 'utf-8')
+        my_parser.set_anno_file(arg_input)
+    elif m_input.suffix == '.json':
+        my_parser = AnnoParser.create_parser('.json', 'utf-8')
+        my_parser.anno_path = arg_input
+    else:
+        raise ValueError("Unsupported Reading method")
+
+    return my_parser
+
+
+def select_writer(arg_output: str, parser: AnnoParser,
+                  binary: bool) -> AnnoWriter:
+    """ Writer selection depending on arg_output """
+    m_output = pathlib.Path(arg_output)
+    if m_output.is_dir():
+        my_writer = AnnoWriter.create_writer(parser, binary, '.xml')
+        my_writer.set_output_folder(arg_output)
+    elif m_output.suffix == '.csv':
+        my_writer = AnnoWriter.create_writer(parser, binary, '.csv')
+        my_writer.set_output_folder(arg_output)
+    else:
+        raise ValueError("Unsupported Writing method")
+
+    return my_writer
+
+
+def main(args=None):
+    """ Main program, as a function to avoid setting up grobal variables """
+    p_args = parse_args(args)
+    my_parser = select_parser(p_args.input)
+    my_data = my_parser.anno_data  # lazily parsing annos on demand
+    my_writer = select_writer(p_args.output, my_parser, p_args.binary)
+    my_writer.write()
+    print_info(my_data)
 
 
 if __name__ == '__main__':
